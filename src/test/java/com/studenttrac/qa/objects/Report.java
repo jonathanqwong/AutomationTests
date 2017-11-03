195779361f33f7cf9102822b45dcb9fd4b4cafa3
RicardoEDI <rmontes@education-dynamics.com>
2017-11-03 15:19:51 -0700

    Report In-Progress
    made base structure classes for Report test automation
    - Report: to create report objects
    - Reports: base testing class
    - ReportPage: functionality for ReportPages

---
+++ src/test/java/com/studenttrac/qa/objects/Report.java
@@ -0,0 +1,13 @@
+package com.studenttrac.qa.objects;
+
+public class Report {
+    int reportId;
+    String groupTitle;
+    String title;
+    String description;
+
+    public Report( int reportId, String groupTitle, String title, String description )
+    {
+
+    }
+}
---
+++ src/test/java/com/studenttrac/qa/webpages/ReportPage.java
@@ -0,0 +1,19 @@
+package com.studenttrac.qa.webpages;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+
+public class ReportPage {
+    WebDriver selenium;
+
+    // Registration Form Page URL
+    private static String REPORT_PAGE = null;
+
+
+    public ReportPage(WebDriver selenium) {
+        this.selenium = selenium;
+    }
+
+
+}
